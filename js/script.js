499a3a5cf1b2d6f0e90fab5babe414a828ac1042
Nikolai Pribitkov <pribitkov2013@mail.ru>
2020-05-10 21:22:45 +0300

    docs: finish project slider-carusel

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next');
+let widthPictures = 134;
+let numberOfVisiblePictures = 3;
+let position = 0;
+let list = document.querySelector('.images');
+let listElements = document.querySelectorAll('li');
+
+prev.onclick = () => {
+    position += widthPictures * numberOfVisiblePictures;
+    position = Math.min(position, 0);
+    list.style.marginLeft = position + 'px';
+};
+
+next.onclick = () => {
+    position -= widthPictures * numberOfVisiblePictures;
+    position = Math.max(position, -widthPictures * (listElements.length - numberOfVisiblePictures));
+    list.style.marginLeft = position + 'px';
+}
